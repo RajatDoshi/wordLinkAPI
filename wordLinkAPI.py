1bddc59782d17e821ebe3a51f8bd0294db2dc9af
rajatdoshi <rajatdoshi@hotmail.com>
2018-02-18 09:01:44 -0500

    Incorporated Angel's Suggestions in API
    Changes:
    1) Rewrote Comments in Code
    2) Reformatted the Output of the returnWordInfo() function
    3) Added Useful Helper Functions
    4) Cleaned Up Excess Parts of the Code

--- wordLinkAPI.py
+++ wordLinkAPI.py
@@ -7,18 +7,18 @@ from collections import OrderedDict
 
 api = FlaskAPI(__name__) 
 
-#local function Section
+"""local function Section"""
 
-# Purporse: Compare Current Part Anotation With Other Parts
-# Input: Other Part's Synsets in the Object that You Are Annotating (key is "otherParts")
-# Output: Returns sorted 
-def returnSortedScores(word, recieve):
+"""
+Purporse: Compare Current Part Anotation With Other Parts
+Input: Other Part's in the Object that You Are Annotating (key for it is "otherParts")
+Output: Returns sorted list 
+"""
+def returnSortedScores(word, data ):
 	scores = []
-	other_parts = {"otherParts": []}
 	synset_with_score = {}
-	for i in recieve["otherPart"]:
-		other_parts["otherParts"].append(i)
-	for other in other_parts["otherParts"]:
+	other_parts = data["otherPart"]
+	for other in other_parts:
 		word_synset = wn.synsets(word)
 		current_synset = wn.synsets(other)
 		for index in range(0, len(current_synset)):
@@ -27,49 +27,28 @@ def returnSortedScores(word, recieve):
 				if val is not None:
 					scores.append(val)
 					synset_with_score[word_synset[indexWord].name()] = val
-	sort_val = sorted(scores, key=float)
-	sor = sorted(synset_with_score.items(), key=operator.itemgetter(1), reverse=True)
-	return sor
-
-
-#This is the function for comparing a part with the other parts 
-#this returns a list of all the synset keys and their similarity scores
-def returnPartComparison(word, recieve):
-	scores = []
-	other_parts = {"otherParts": []}
-	synset_with_score = {}
-	for i in recieve["otherPart"]:
-		other_parts["otherParts"].append(i)
-	for other in other_parts["otherParts"]:
-		word_synset = wn.synsets(word)
-		current_synset = wn.synsets(other)
-		for index in range(0, len(current_synset)):
-			for indexWord in range (0, len(word_synset)):
-				val = current_synset[index].wup_similarity(word_synset[indexWord])
-				if val is not None:
-					scores.append(val)		
-					synset_with_score[word_synset[indexWord].name()] = val				
-	return synset_with_score
-
-#This funtion returns name, definition, POS, and synset key of a word in a list
+	sorted_scores = sorted(scores, key=float)
+	sortedList = sorted(synset_with_score.items(), key=operator.itemgetter(1), reverse=True)
+	return sortedList
+"""
+Purpose: Function For Getting Info About a Word
+Input: The Common Name of a Part
+Output: Returns Information of the part as a dictionary inside 1 list
+"""
 def returnWordInfo(word):
 	syn = wordnet.synsets(word)
-	length = len(syn)
-	name = [
-	{"synsetKey": []},
-	{"synsetDefinition": []},
-	{"synsetPOS": []},
-	{"synsetCommonName": []}
-	]
+	info = []
+	b = {}
+	j = 0
 	for i in syn:
-		for j in i.lemmas():
-			name[0]["synsetKey"].append(i.name())
-			name[1]["synsetDefinition"].append(i.definition())
-			name[2]["synsetPOS"].append(i.pos())
-			name[3]["synsetCommonName"].append(j.name())
-	return name
+		info.append(b)
+		info[j]["Key"] = i.name()
+		info[j]["SynsetID"] = i.offset()
+		info[j]["SynsetGloss"] = i.definition()
+		j = j + 1
+	return info
 
-#funtion to return a list of definitions for every synset of a particular word
+"""Return List of Definitions for a Word"""
 def returnDef(word):
 	syn = wordnet.synsets(word)
 	length = len(syn)
@@ -78,16 +57,16 @@ def returnDef(word):
 		definitionList.append(i.definition())
 	return definitionList
 
-#function to return a list of a word's synset IDs
-def returnID(word):
+"""Return List of Synset Keys for a Word"""
+def returnKey(word):
 	syn = wordnet.synsets(word)
 	length = len(syn)
-	idList = []
+	keyList = []
 	for i in syn:
-		idList.append(i.offset())
-	return idList
+		keyList.append(i.name())
+	return keyList
 
-#function that returns a list of part of speechs for each synset of a word
+"""Return List of POS for Every Synset of a Word"""
 def returnPOS(word):
 	syn = wordnet.synsets(word)
 	length = len(syn)
@@ -96,43 +75,49 @@ def returnPOS(word):
 		posList.append(i.pos())
 	return posList
 
-#main screen
+"""Return List of Synset IDs for a Word"""
+def returnID(word):
+	syn = wordnet.synsets(word)
+	length = len(syn)
+	idList = []
+	for i in syn:
+		idList.append(i.offset())
+	return idList
+
+"""main screen"""
 @api.route("/", methods=['GET', 'POST'])
 def test():
 	return jsonify ({'message' : 'It works'})
 
-#test different functions at this address
+"""Adress For Testing Helper Functions"""
 @api.route('/wordlink/currentTest', methods=['GET', 'POST'])
 def testThings ():
-	recieve = request.get_json()
-	otherPartList = {"otherPart": recieve["otherPart"]}
-	word = 	recieve["currentPart"]
+	data = request.get_json()
+	otherPartList = {"otherPart": data["otherPart"]}
+	word = 	data["currentPart"]
 	synset_with_score = returnSortedScores(word, otherPartList)
 	return jsonify(synset_with_score)
 
-#adress to get information about a synset 
+""" 
+Purpose: Adress For Getting Info About a Word
+Input: The Common Name of a Part (dictionary key is "part")
+Output: Returns Information of the part as a dictionary inside 1 list
+"""
 @api.route('/wordlink/getInfo', methods=['GET', 'POST'])
 def returnInfo():
-	recieve = request.get_json()
-	info = []
-	for i in range (0, len(recieve["part"])):
-		word = recieve["part"][i]
-		info.append(returnWordInfo(word))
-	return jsonify(info)
-
-@api.route('/wordlink/compare', methods=['GET', 'POST'])
-def compare():
-	recieve = request.get_json()
-	otherPartList = {"otherPart": recieve["otherPart"]}
-	word = 	recieve["currentPart"]
-	synset_with_score = returnPartComparison(word, otherPartList)
-	return jsonify(synset_with_score)
-
+	data = request.get_json()
+	word = data["part"][0]
+	return jsonify(returnWordInfo(word))
+""" 
+Purpose: Adress For Identifying Correct Synset for a Part Annotation Linkage Based on Similarity Scores
+Input: Current Part and List of Other Parts in an Object (keys are "otherPart" and "currentPart")
+Output: Returns Sorted Dictionary 
+"""
 @api.route('/wordlink/compare_and_sort', methods=['GET', 'POST'])
 def sort():
-	recieve = request.get_json()
-	otherPartList = {"otherPart": recieve["otherPart"]}
-	word = 	recieve["currentPart"]
+	data = request.get_json()
+	otherPartList = {"otherPart": data["otherPart"]}
+	word = 	data["currentPart"]
 	sorted_synsets = returnSortedScores(word, otherPartList)
 	return jsonify(sorted_synsets)
 
