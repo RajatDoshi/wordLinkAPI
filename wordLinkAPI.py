dfd1e6cd752544359f222344d1c52c47a9f1a6ed
rajatdoshi <rajatdoshi@hotmail.com>
2018-02-19 17:51:00 -0500

    Fixed Bug in returnWordInfo()
    1) Appended a dictionary into the list each iteration
    2) Made Code Cleaner

--- wordLinkAPI.py
+++ wordLinkAPI.py
@@ -17,7 +17,7 @@ Output: Returns sorted list
 def returnSortedScores(word, data ):
 	scores = []
 	synset_with_score = {}
-	other_parts = data["otherPart"]
+	other_parts = data
 	for other in other_parts:
 		word_synset = wn.synsets(word)
 		current_synset = wn.synsets(other)
@@ -38,16 +38,13 @@ Output: Returns Information of the part as a dictionary inside 1 list
 def returnWordInfo(word):
 	syn = wordnet.synsets(word)
 	info = []
-	b = {}
-	j = 0
-	for i in syn:
-		info.append(b)
-		info[j]["Key"] = i.name()
-		info[j]["SynsetID"] = i.offset()
-		info[j]["SynsetGloss"] = i.definition()
-		j = j + 1
+	bracket = {}
+	for j in range(0,len(syn)):
+		info.append({})
+		info[j].update({"Key": syn[j].name()})
+		info[j].update({"SynsetGloss": syn[j].definition()})
+		info[j].update({"SynsetID": syn[j].offset()})
 	return info
-
 """Return List of Definitions for a Word"""
 def returnDef(word):
 	syn = wordnet.synsets(word)
@@ -100,24 +97,26 @@ def testThings ():
 
 """ 
 Purpose: Adress For Getting Info About a Word
-Input: The Common Name of a Part (dictionary key is "part")
+Input: The Common Name of a Part (make sure item is a list) (dictionary key is "label")
 Output: Returns Information of the part as a dictionary inside 1 list
 """
 @api.route('/wordlink/getInfo', methods=['GET', 'POST'])
 def returnInfo():
 	data = request.get_json()
-	word = data["part"][0]
-	return jsonify(returnWordInfo(word))
+	word = data["label"]
+	output = returnWordInfo(word)
+	return jsonify(output)
+
 """ 
 Purpose: Adress For Identifying Correct Synset for a Part Annotation Linkage Based on Similarity Scores
-Input: Current Part and List of Other Parts in an Object (keys are "otherPart" and "currentPart")
+Input: Current Part and List of Other Parts in an Object (keys are "otherPart" and "label")
 Output: Returns Sorted Dictionary 
 """
 @api.route('/wordlink/compare_and_sort', methods=['GET', 'POST'])
 def sort():
 	data = request.get_json()
-	otherPartList = {"otherPart": data["otherPart"]}
-	word = 	data["currentPart"]
+	otherPartList = data["otherPart"]
+	word = 	data["label"]
 	sorted_synsets = returnSortedScores(word, otherPartList)
 	return jsonify(sorted_synsets)
 
